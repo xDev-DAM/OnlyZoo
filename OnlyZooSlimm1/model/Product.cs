da272c92281995c56759054f8ffa95d7a095427c
Vespertino <Vespertino@C09PC06.informatica.comercio>
2023-11-27 17:02:22 +0100

    añadidos modelos para producto, listaProducto, especie y listaEspecies. Pendiente revisión darme feedback :)

---
+++ OnlyZooSlimm1/model/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.model
+{
+    internal class Product
+    {
+        public int Id;
+        private string Name;
+        private string Description;
+        private double Prize;
+        private int Stock;
+        private Specie ProductSpecie;
+
+        public Product(int Id,string Name, string Description, double Prize, int Stock, Specie ProductSpecie) { 
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
+            this.Prize = Prize;
+            this.Stock = Stock;
+            this.ProductSpecie = ProductSpecie;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Product product &&
+                   Id == product.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
