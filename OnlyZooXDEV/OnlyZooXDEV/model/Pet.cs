3d11f4e539387c44cbd54ed2ebe4cf0b49b8d381
Vespertino <Vespertino@C09PC06.informatica.comercio>
2023-11-29 21:35:29 +0100

    remplazo rama main

---
+++ OnlyZooXDEV/OnlyZooXDEV/model/Pet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo.model
+{
+    internal class Pet
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Breed { get; set; }
+        private string Description { get; set; }
+        private Specie PetSpecie { get; set; }
+        private int Stock { get; set; }
+        private string Image { get; set; }
+        private double Prize { get; set; }
+        
+        public Pet(int Id, string Name, string Breed, string Description, Specie PetSpecie,int stock,string Image, double Prize ) {
+            this.Id = Id;
+            this.Name = Name;
+            this.Breed = Breed;
+            this.Description = Description;
+            this.PetSpecie = PetSpecie;
+            this.Stock = Stock;
+            this.Image = Image;
+            this.Prize = Prize;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Pet pet &&
+                   Id == pet.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
