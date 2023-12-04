7ad0b9444a2a71c1aa53a634003e6080ff8793ec
slimm1 <tini.ramonda@gmail.com>
2023-12-04 02:16:55 +0100

    pequeña correcion en panel principal y añadido ejemplo de carga de items aleatorios

--- OnlyZooXDEV/OnlyZooXDEV/MainWindow.cs
+++ OnlyZooXDEV/OnlyZooXDEV/MainWindow.cs
@@ -30,6 +30,7 @@ namespace OnlyZooXDEV
         {
             LoadFonts(new FontManager());
             LoadColors();
+            LoadItems();
         }
 
         private void LoadFonts(FontManager f)
@@ -55,6 +56,21 @@ namespace OnlyZooXDEV
             this.SpecieFilter.BackColor = Color.White;
             this.MainPanel.BackColor = ColorManager.GetPaletteColor3();
             this.MainBotPanel.BackColor = ColorManager.GetPaletteColor1();
+        }
+        // una prueba para medidas!
+        private void LoadItems() {
+            for (int i = 0; i < 50; i++)
+            {
+                CreateButton();
+            }
+        }
+
+        private void CreateButton()
+        {
+            Button b = new Button();
+            b.Size = new Size(210, 210);
+            b.Margin = new Padding(20, 20, 3, 3);
+            b.Parent = this.ItemContainer;
         }
     }
 }
